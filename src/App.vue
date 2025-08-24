e7d9851da1841018dc1788450fdd66250d54e8ae
fahmid <dnetfahmidhasan@gmail.com>
2025-08-24 17:31:40 +0600

    herosection built start

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { defineAsyncComponent } from 'vue';
-const Navbar = defineAsyncComponent(()=>import('@/components/layout/Navbar.vue'))
+const Navbar = defineAsyncComponent(()=>import('@/components/layout/Navbar.vue'));
+const HeroSection = defineAsyncComponent(()=>import('@/components/HeroSection.vue'))
 
 </script>
 
 <template>
   <div class="bg-[#D4EBF8] dark:bg-[#080326] min-h-screen">
-    <Navbar />    
+    <Navbar />  
+    <HeroSection />  
   </div>
 </template>
 
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="flex justify-center items-center dark:text-white main-h-screen mt-20 lg:mt-0">
+        <header
+            class="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#00c6cc] 
+            via-[#785ae4] to-color_blue opacity-30 dark:opacity-20 blur-[100p] left-10 top-0 hidden md:block">
+        </header>
+        <header
+            class="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#00c6cc] 
+            via-[#785ae4] to-color_blue opacity-30 dark:opacity-20 blur-[100p] right-10 bottom-0 hidden md:block">
+        </header>
+
+        <main class="absolute grid lg:grid-cols-2 grid-cols-1 items-center p-2 lg:p-8">
+            <header class="text-center lg:text-left">
+                <h1 class="mb-6 text-4xl md:text-5xl lg:text-6xl font-bold">
+                    Hi, I'm <span class="text-color_blue stroke-text text-stroke-color_blue dark:text-stroke-secondary">Hasan</span>
+                </h1>
+            </header>
+        </main>
+    </section>
+</template>
+
+
+<script>
+
+</script>
+<style setup></style>

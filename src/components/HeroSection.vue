b3c5c43a17d3aa6012f3d07db9f3c4ac43c71550
fahmid <dnetfahmidhasan@gmail.com>
2025-08-26 17:06:57 +0600

    Button

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,30 +1,36 @@
 <template>
     <section class="flex justify-center items-center dark:text-white min-h-screen mt-20 lg:mt-0">
-        <header
-            class="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#00c6cc] 
+        <header class="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#00c6cc] 
             via-[#785ae4] to-secondary opacity-30 dark:opacity-20 blur-[100p] left-10 top-0 hidden md:block">
         </header>
-        <header
-            class="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#00c6cc] 
+        <header class="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#00c6cc] 
             via-[#785ae4] to-secondary opacity-30 dark:opacity-20 blur-[100p] right-10 bottom-0 hidden md:block">
         </header>
 
         <main class="absolute grid lg:grid-cols-2 grid-cols-1 items-center p-2 lg:p-8">
             <header class="text-center lg:text-left">
                 <h1 class="mb-6 text-4xl md:text-5xl lg:text-6xl font-bold">
-                    👋Hi, I'm <span class="text-secondary stroke-text text-stroke-primary dark:text-stroke-secondary">Hasan</span>
+                    👋Hi, I'm <span
+                        class="text-secondary stroke-text text-stroke-primary dark:text-stroke-secondary">Hasan</span>
                 </h1>
+                <p class="text-lg max-w-xl mx-auto lg:mx-0 font-semibold">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime repudiandae corrupti fuga rerum
+                    explicabo doloribus possimus laborum, asperiores deserunt eaque!
+                </p>
+                <div class="mt-6 flex justify-center lg:justify-start">
+                    <Button label="Hire Me"/>
+                </div>
             </header>
         </main>
     </section>
 </template>
 
 
-<script>
+<script setup>
+
+import Button from '@/components/UI/Button.vue'
 
 </script>
 
 
-<style setup>
-
-</style>
+<style setup></style>
